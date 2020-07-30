07c8a56c6261b7a0764c22fe96d3212a67d46530
Shantanu Singh <47878607+Shantanu2k19@users.noreply.github.com>
2020-07-30 19:44:20 +0530

    adding/importing data from .csv file

---
+++ sql_python/create_table/import.py
@@ -0,0 +1,26 @@
+#CODE 3
+
+#adding(importing) data from csv fileimport csv
+import os, csv
+
+from flask import Flask, render_template, request
+from models import *
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = ("postgres://zodiac:elonmusk@localhost/postgres")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+def main():
+    f = open("flights.csv")
+    reader = csv.reader(f)
+    for origin, destination, duration in reader:
+        obj = flight(origin=origin, destination=destination, duration=duration) #flight is the name of the class(model.py)
+        #flight object
+        db.session.add(obj) #equivalent to insert command
+        print(f"Added flight from {origin} to {destination} lasting {duration} minutes.")
+    db.session.commit() #apply changes
+
+if __name__ == "__main__":
+    with app.app_context():
+        main()
