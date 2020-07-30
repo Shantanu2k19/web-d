76398877c586d1aeae9cedd2ac062352583b3910
Shantanu Singh <47878607+Shantanu2k19@users.noreply.github.com>
2020-07-30 19:50:24 +0530

    Add files via upload

---
+++ sql_python/airline1/application.py
@@ -0,0 +1,112 @@
+from flask import Flask, render_template, request, jsonify 
+from models import *
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = ("postgres://zodiac:elonmusk@localhost/postgres")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+"""
+here, 
+Flight : name of class for creating table
+Passengers : name of class for creating passengers table
+
+flightss : object/variable used to access data 
+"""
+
+@app.route("/")
+def index():
+    flightss = Flight.query.all()
+    return render_template("index.html", flights=flightss)
+
+
+@app.route("/book", methods=["POST"])
+def book():
+    """Book a flight."""
+
+    # Get form information.
+    name = request.form.get("name")
+    try:
+        flight_id = int(request.form.get("flight_id"))
+    except ValueError:
+        return render_template("error.html", message="Invalid flight number.")
+
+    # Make sure the flight exists.
+    flight = Flight.query.get(flight_id)
+    if flight is None:
+        return render_template("error.html", message="No such flight with that id.")
+
+    # Adding a passenger.
+
+    #way 1, using python replacement of sql, like we did previously
+    passenger = Passenger(name=name, flight_id=flight_id)
+    db.session.add(passenger)
+    db.session.commit()
+
+    #way 2, using class,
+    #flight.add_passenger(name) #uncomment to use 2nd way
+    #here, add_passenger is a definition added in the class Flight(models.py)
+    #takes name,adds and commit it
+
+    
+    return render_template("success.html")
+
+
+@app.route("/flights")
+def flights():
+    """List all flights."""
+    flights = Flight.query.all()
+    return render_template("flights.html", flights=flights)
+
+
+@app.route("/flights/<int:flight_id>")
+def flight(flight_id):
+    """List details about a single flight."""
+
+    # Make sure flight exists.
+    flight = Flight.query.get(flight_id)
+    if flight is None:
+        return render_template("error.html", message="No such flight.")
+
+    # Get all passengers.
+
+    #way 1
+    #passengers = Passenger.query.filter_by(flight_id=flight_id).all()
+
+    #way2 : using relationships(check models.py)
+    passengers = flight.passengers
+
+    #printing fetched details
+    return render_template("flight.html", flight=flight, passengers=passengers)
+
+
+
+
+#adding a route for API
+@app.route("/api/flights/<int:flight_id>")
+def flight_api(flight_id):
+    """Return details about a single flight."""
+
+    # Make sure flight exists.
+    flight = Flight.query.get(flight_id)
+    if flight is None:
+        return jsonify({"error": "Invalid flight_id"}), 422
+        #jsonify function used to return jason object {}, 422-> pass back an error status code
+
+
+    # Get all passengers.
+    passengers = flight.passengers
+    #create a list
+    names = []
+    for passenger in passengers:
+        names.append(passenger.name)
+    #add all passengers to the list 
+
+    #return jason object
+    return jsonify({
+            "origin": flight.origin,
+            "destination": flight.destination,
+            "duration": flight.duration,
+            "passengers": names
+        })
---
+++ sql_python/airline1/models.py
@@ -0,0 +1,32 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Flight(db.Model):
+    __tablename__ = "flights"
+    id = db.Column(db.Integer, primary_key=True)
+    origin = db.Column(db.String, nullable=False)
+    destination = db.Column(db.String, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+
+    #added relationships (its all about simplifying the syntax)
+    passengers = db.relationship("Passenger", backref="flight", lazy=True)
+    #relating both tables, flight is the keyword to access the passenger details
+    #lazy evaluate tells database to not fetch the passengers info unless called(effeciency bonus)
+
+
+    def add_passenger(self, name):
+        p = Passenger(name=name, flight_id=self.id)
+        db.session.add(p)
+        db.session.commit()
+
+
+class Passenger(db.Model):
+    __tablename__ = "passengers"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    flight_id = db.Column(db.Integer, db.ForeignKey("flights.id"), nullable=False)
