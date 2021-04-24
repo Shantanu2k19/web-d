b89708609ca7cdc8cdc89be425639148284e67e4
Shantanu2k19 <shantanusingh2030@gmail.com>
2021-04-24 09:57:46 +0530

    added name on line 10 testing

--- flask_app/notes/application.py
+++ flask_app/notes/application.py
@@ -7,6 +7,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+#shantanu singh
 #notes global variable,hence common to all the users on server
 #notes = []
 #should be avoided, hence concept of sessions
