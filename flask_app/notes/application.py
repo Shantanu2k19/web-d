d00b23e4edbc5d9274903fb614ed184b4a6cf55c
Shantanu2k19 <shantanusingh2030@gmail.com>
2021-04-24 16:29:17 +0530

    added css to layout.html

--- flask_app/notes/application.py
+++ flask_app/notes/application.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, session
 from flask_session import Session
 #session data stored about server side
 app = Flask(__name__)
-
+#app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
@@ -21,3 +21,10 @@ def index():
 		note = request.form.get("note")
 		session["notes"].append(note)
 	return render_template("index.html", notes=session["notes"])
+
+@app.route('/_get_ajax_data/')
+def get_ajax_data():
+    data = {"hello": "world"}
+    response = jsonify(data)
+    response.cache_control.max_age = 60 * 60 * 24  # 1 day (in seconds)
+    return response
